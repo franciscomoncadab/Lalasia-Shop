b36ccce8cbaf93091c929b599653ed3da9cdd208
Francisco Moncada B <fmoncadabuelvas0112@gmail.com>
2022-12-05 21:35:59 -0500

    add tailwind and scss dependencies

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import './App.css';
+import Footer from './components/Footer';
 import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div>
       <NavBar />
+      <Footer />
     </div>
   );
 }
